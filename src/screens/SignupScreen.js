4f14630ace951bc55879650e7bd9b9d1d488244d
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-03 10:01:32 +1000

    added react-native-element dependencies & signup page styling

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from './components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-    return <>
-        <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-        <Button title='Go to Signin' onPress={() => navigation.navigate('Signin')} />
-        <Button title='Go to main flow' onPress={() => navigation.navigate('mainFlow')} />
-
-    </>
+    return (
+        <View style={styles.container}>
+            <Text style={styles.header} h3>Sign Up for Tracker</Text>
+            <Spacer>
+                <Input style={styles.input} label="Email" />
+                <Input style={styles.input} label="Password" />
+            </Spacer>
+            <Button style={styles.button} title="Sign Up" />
+        </View>
+    )
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 250,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    header: {
+        marginLeft: 10,
+        marginRight: 10
+    },
+    button: {
+        marginLeft: 10,
+        marginRight: 10
+    }
+});
 
 export default SignupScreen;
