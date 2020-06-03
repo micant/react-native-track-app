3915a1a544a1b90b0266950bb2ed90333319c78f
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-03 11:35:45 +1000

    authentication context WIP

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
@@ -7,8 +8,34 @@ const authReducer = (state, action) => {
     }
 };
 
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        /// make api request to sign up with email & password
+
+        // if we sign up, modify our state and say that we're authenticated
+
+        // plus error handling
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // try to sign in
+
+        // handle success up updating state
+
+        // error handling
+    };
+} ;
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out??
+    };
+};
+
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signup, signin, signout },
     { isSignedIn: false }
 );
