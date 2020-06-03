95b43d480c4448072443d15691be6146542f57eb
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-03 10:11:37 +1000

    setup state and input props on signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from './components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
     return (
         <View style={styles.container}>
             <Text style={styles.header} h3>Sign Up for Tracker</Text>
             <Spacer>
-                <Input style={styles.input} label="Email" />
-                <Input style={styles.input} label="Password" />
+                <Input 
+                    style={styles.input} 
+                    label="Email" value={email} 
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+                <Input 
+                    secureTextEntry
+                    style={styles.input} 
+                    label="Password" 
+                    onChangeText={setPassword}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
             </Spacer>
             <Button style={styles.button} title="Sign Up" />
         </View>
