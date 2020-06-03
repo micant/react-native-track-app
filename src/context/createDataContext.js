67e26cd6c5bb1dbf38f6f6242967b4cf2cb24f2a
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-03 10:29:39 +1000

    added authentication context provider

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
 
-export default (reducer, action, defaultValue) => {
+export default (reducer, actions, defaultValue) => {
     const Context = React.createContext();
 
     const Provider = ({ children }) => {
