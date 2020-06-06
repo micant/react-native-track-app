c4ab9f3300c924c6beac66d0f11a5a106a98ede7
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-06 13:09:34 +1000

    added user location data context and provider

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
   createAppContainer,
   createSwitchNavigator
 } from 'react-navigation';
@@ -15,6 +15,8 @@ import TrackListScreen from './src/screens/TrackListScreen';
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
+import { Provider as LocationProvider } from './src/context/LocationContext';
+
 import { setNavigator } from './src/navigationRef';
 
 const switchNavigator = createSwitchNavigator({
@@ -37,8 +39,10 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <AuthProvider>
-      <App ref={(navigator) => { setNavigator(navigator) }}/>
-    </AuthProvider>
+    <LocationProvider>
+      <AuthProvider>
+        <App ref={(navigator) => { setNavigator(navigator) }} />
+      </AuthProvider>
+    </LocationProvider>
   );
 };
