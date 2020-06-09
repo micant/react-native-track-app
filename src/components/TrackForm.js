7063883cfcffa05670e671e2760080296c983b18
Michael Antoniades <mic.antoniades@gmail.com>
2020-06-09 13:40:24 +1000

    added action functions for name and locations arr

---
+++ src/components/TrackForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Input, Button } from 'react-native-elements';
+import Spacer from './Spacer';
+
+const TrackForm = () => {
+    return (
+        <>
+            <Spacer>
+                <Input placeholder="Enter track name" />
+                <Button title="Start Recording" />
+            </Spacer>
+        </>
+    )
+};
+
+export default TrackForm;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -4,18 +4,23 @@ import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
-import { Context as LocationContext } from '../context/LocationContext'; 
+import TrackForm from '../components/TrackForm';
+
+import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = ({ isFocused }) => {
-    const { addLocation } = useContext(LocationContext);
-    const [err] = useLocation(isFocused, addLocation);
+    const { state, addLocation } = useContext(LocationContext);
+    const [err] = useLocation(isFocused, location => {
+        addLocation(location, state.recording);
+    });
 
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a track</Text>
             <Map />
             {err ? <Text>Please enable location services</Text> : null}
+            <TrackForm />
         </SafeAreaView>
     )
 };
